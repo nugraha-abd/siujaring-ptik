d9fdd06b03deb970babe99e4d2d88cbcf9b321fb
nugraha-abd <nugrahaabdurahman@gmail.com>
2021-09-20 11:53:33 +0700

    Add database config

---
+++ config/db.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize')
+
+const database = new Sequelize('siujaring', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+})
+
+// DB authentication
+database
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.')
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err)
+  })
+
+module.exports = database
