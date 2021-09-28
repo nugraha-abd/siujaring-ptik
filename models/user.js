632a60c6bfaac893e3b8d0fecf05109677f80250
nugraha-abd <nugrahaabdurahman@gmail.com>
2021-09-28 22:58:04 +0700

    Add association models and set allow null to false for certain tables

--- models/user.js
+++ models/user.js
@@ -1,22 +1,29 @@
 const Sequelize = require('sequelize')
 const db = require('../config/db')
 
+const Mahasiswa = require('./mahasiswa')
+const Dosen = require('./dosen')
+
 const User = db.define('tb_user', {
   id_user: {
     type: Sequelize.INTEGER(4),
     primaryKey: true,
     autoIncrement: true,
     allowNull: false,
+    defaultValue: null,
   },
   username: {
     type: Sequelize.STRING(15),
     unique: true,
+    allowNull: false,
   },
   role: {
     type: Sequelize.ENUM('dosen', 'mahasiswa', 'admin'),
+    allowNull: false,
   },
   password: {
     type: Sequelize.STRING(30),
+    allowNull: false,
   },
   keterangan: {
     type: Sequelize.STRING(256),
@@ -25,10 +32,21 @@ const User = db.define('tb_user', {
   email: {
     type: Sequelize.STRING(100),
     unique: true,
+    allowNull: false,
   },
-  createdAt: Sequelize.DATE,
-  updatedAt: Sequelize.DATE,
-  underscored: true,
+  created_at: {
+    type: Sequelize.DATE,
+    allowNull: false,
+    defaultValue: Sequelize.NOW,
+  },
+  updated_at: {
+    type: Sequelize.DATE,
+    allowNull: false,
+    defaultValue: Sequelize.NOW,
+  },
 })
+
+User.hasOne(Mahasiswa)
+User.hasOne(Dosen)
 
 module.exports = User
