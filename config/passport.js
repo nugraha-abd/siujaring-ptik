cef9d12385ca69dc50c3245e752611087b462d3d
nugraha-abd <nugrahaabdurahman@gmail.com>
2021-09-30 00:34:46 +0700

    Add passport config

---
+++ config/passport.js
@@ -0,0 +1,30 @@
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+const fs = require('fs')
+const path = require('path')
+const User = require('../models/user')
+
+const PUB_KEY = fs.readFileSync(
+  path.join(__dirname, '..', 'public.pem'),
+  'utf8'
+)
+
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: PUB_KEY,
+  algorithms: ['ES512'],
+}
+
+module.exports = (passport) => {
+  passport.use(
+    new JwtStrategy(options, async (jwt_payload, done) => {
+      try {
+        let user = await User.findOne({ where: { id_user: jwt_payload.sub } })
+        if (user) return done(null, user)
+        return done(null, false)
+      } catch (err) {
+        return done(err, false)
+      }
+    })
+  )
+}
