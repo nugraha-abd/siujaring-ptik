7d718e702a34879cfc7f71cda933ff2ba1ba2957
nugraha-abd <nugrahaabdurahman@gmail.com>
2021-09-29 10:16:12 +0700

    Remove comments which no longer needed

--- routes/login.js
+++ routes/login.js
@@ -3,19 +3,6 @@ const router = express.Router()
 
 const loginController = require('../controller/login')
 
-// router
-//   .route('/:id')
-//   .get((req, res) => {
-//     console.log(req.user)
-//     res.send(`Get User With ID ${req.params.id}`)
-//   })
-//   .put((req, res) => {
-//     res.send(`Update User With ID ${req.params.id}`)
-//   })
-//   .delete((req, res) => {
-//     res.send(`Delete User With ID ${req.params.id}`)
-//   })
-
 router.post('/', loginController.login)
 
 module.exports = router
