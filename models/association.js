9bbb3f85b17428e1125a71feecb4086d413a02c9
nugraha-abd <nugrahaabdurahman@gmail.com>
2021-10-09 13:34:36 +0700

    Add N:M association models

--- models/association.js
+++ models/association.js
@@ -60,7 +60,7 @@ function association(sequelize) {
 
   // MataKuliah - KodeSeksi [N:1]
   MataKuliah.hasMany(KodeSeksi, {
-    as: 'mata-kuliah',
+    as: 'kode_seksi',
     foreignKey: 'id_matkul',
     sourceKey: 'id_matkul',
   })
@@ -71,13 +71,116 @@ function association(sequelize) {
 
   // Semester - KodeSeksi [N:1]
   Semester.hasMany(KodeSeksi, {
-    as: 'semester',
+    as: 'kode_seksi',
     foreignKey: 'id_semester',
     sourceKey: 'id_semester',
   })
   KodeSeksi.belongsTo(Semester, {
     foreignKey: 'id_semester',
     targetKey: 'id_semester',
+  })
+
+  // JenisUjian - KodeSeksi [N:1] - ask later
+  KodeSeksi.hasMany(JenisUjian, {
+    as: 'jenis_ujian',
+    foreignKey: 'id_kosek',
+    sourceKey: 'id_kosek',
+  })
+  JenisUjian.belongsTo(KodeSeksi, {
+    foreignKey: 'id_kosek',
+    targetKey: 'id_kosek',
+  })
+
+  // Dosen - SoalPg [N:1]
+  Dosen.hasMany(SoalPg, {
+    as: 'soal_pg',
+    foreignKey: 'id_dosen',
+    sourceKey: 'id_dosen',
+  })
+  SoalPg.belongsTo(Dosen, {
+    foreignKey: 'id_dosen',
+    targetKey: 'id_dosen',
+  })
+
+  // MataKuliah - SoalPg [1:1]
+  MataKuliah.hasOne(SoalPg, {
+    as: 'soal_pg',
+    foreignKey: 'id_matkul',
+    sourceKey: 'id_matkul',
+  })
+  SoalPg.belongsTo(MataKuliah, {
+    foreignKey: 'id_matkul',
+    targetKey: 'id_matkul',
+  })
+
+  // JenisUjian - PaketSoal [1:1]
+  JenisUjian.hasOne(PaketSoal, {
+    as: 'jenis_ujian',
+    foreignKey: 'id_matkul',
+    sourceKey: 'id_matkul',
+  })
+  PaketSoal.belongsTo(JenisUjian, {
+    foreignKey: 'id_matkul',
+    targetKey: 'id_matkul',
+  })
+
+  // KodeSeksi - PaketSoal [1:1]
+  KodeSeksi.hasOne(PaketSoal, {
+    as: 'kode_seksi',
+    foreignKey: 'id_jenis_ujian',
+    sourceKey: 'id_jenis_ujian',
+  })
+  PaketSoal.belongsTo(KodeSeksi, {
+    foreignKey: 'id_jenis_ujian',
+    targetKey: 'id_jenis_ujian',
+  })
+
+  // Mahasiswa - PaketSoal [N:M]
+  Mahasiswa.belongsToMany(PaketSoal, {
+    through: 'RelPaketSoalMahasiswa',
+    sourceKey: 'id_mhs',
+    targetKey: 'id_mhs',
+  })
+  PaketSoal.belongsToMany(Mahasiswa, {
+    through: 'RelPaketSoalMahasiswa',
+    sourceKey: 'id_paket',
+    targetKey: 'id_paket',
+  })
+
+  // Mahasiswa - KodeSeksi [N:M]
+  Mahasiswa.belongsToMany(KodeSeksi, {
+    through: 'RelKodeSeksiMahasiswa',
+    sourceKey: 'id_mhs',
+    targetKey: 'id_mhs',
+  })
+  KodeSeksi.belongsToMany(Mahasiswa, {
+    through: 'RelKodeSeksiMahasiswa',
+    sourceKey: 'id_kosek',
+    targetKey: 'id_kosek',
+  })
+
+  // Soal - PaketSoal [N:M]
+  Soal.belongsToMany(PaketSoal, {
+    through: 'RelSoalPaketSoal',
+    sourceKey: 'id_soal',
+    targetKey: 'id_soal',
+  })
+  PaketSoal.belongsToMany(Soal, {
+    through: 'RelSoalPaketSoal',
+    sourceKey: 'id_paket',
+    targetKey: 'id_paket',
+  })
+
+  // KodeSeksi - PaketSoal [N:M]
+  KodeSeksi.belongsToMany(PaketSoal, {
+    through: 'RelKodeSeksiPaketSoal',
+    sourceKey: 'id_kosek',
+    targetKey: 'id_kosek',
+  })
+  PaketSoal.belongsToMany(KodeSeksi, {
+    through: 'RelKodeSeksiPaketSoal',
+    sourceKey: 'id_paket',
+    targetKey: 'id_paket',
   })
 }
 
