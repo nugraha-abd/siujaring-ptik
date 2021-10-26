24b24c8fd08e53cd37ebefcf51bc1b524e401ca5
nugraha-abd <nugrahaabdurahman@gmail.com>
2021-10-26 14:16:56 +0700

    Add routes for mata-kuliah, kode-seksi, semester, and soal-pg

--- app.js
+++ app.js
@@ -17,12 +17,28 @@ app.get('/', (req, res) => res.send('Node berhasil dibuka pada REST API'))
 const loginRoutes = require('./routes/login')
 app.use('/login', loginRoutes)
 
-// Passport-jwt authentication middleware (executed after successful login)
+// Passport-jwt authentication middleware (executed after successful login attempt)
 app.use(passport.authenticate('jwt', { session: false }))
 
-// User routes
+// User Routes
 const userRoutes = require('./routes/user')
 app.use('/user', userRoutes)
+
+// Mata Kuliah Routes
+const mataKuliahRoutes = require('./routes/mata-kuliah')
+app.use('/user', mataKuliahRoutes)
+
+// Kode Seksi Routes
+const kodeSeksiRoutes = require('./routes/kode-seksi')
+app.use('/user', kodeSeksiRoutes)
+
+// Semester Routes
+const semesterRoutes = require('./routes/semester')
+app.use('/user', semesterRoutes)
+
+// Soal Pg Routes
+const soalPgRoutes = require('./routes/soal-pg')
+app.use('/user', soalPgRoutes)
 
 const PORT = process.env.PORT || 5000
 
