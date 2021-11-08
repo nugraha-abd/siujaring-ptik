29381a00f26ee7ce79e43438415049b74d79e982
nugraha-abd <nugrahaabdurahman@gmail.com>
2021-11-08 13:05:16 +0700

    Move login and logout route into auth routes file

--- routes/login.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const loginController = require('../controller/login')
-
-router.post('/', loginController.login)
-
-module.exports = router
--- routes/logout.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const logoutController = require('../controller/logout')
-
-router.post('/', logoutController.logout)
-
-module.exports = router
