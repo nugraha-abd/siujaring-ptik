1b9ab87c90042c29547737542d759c0cf9abbbdd
nugraha-abd <nugrahaabdurahman@gmail.com>
2021-11-09 21:45:42 +0700

    Change import variable to destructuring object

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const checkDuplicate = require('../middleware/checkDuplicate')
+const { checkDuplicate } = require('../middleware/checkDuplicate')
 
 const userController = require('../controller/user')
 
