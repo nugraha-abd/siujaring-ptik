de53c43b44578ce8d86b720a3ee381aee35acb76
dhirgham <dhirgham@dhirghams-MacBook-Air.local>
2021-11-16 21:06:38 +0700

    Code refactor for authorization access error handling

--- controller/mata-kuliah.js
+++ controller/mata-kuliah.js
@@ -3,28 +3,27 @@ const { models } = require('../models/index')
 module.exports = {
   get: async (req, res) => {
     try {
-      if (req.user.role == 'admin') {
-        const data = await models.MataKuliah.findAll({
-          attributes: {
-            exclude: ['id_matkul'],
-          },
-        })
-
-        if (data.length === 0) {
-          return res
-            .status(404)
-            .json({ message: 'Data mata kuliah tidak ditemukan' })
-        }
-
-        res.status(200).json({
-          message: 'Data seluruh mata kuliah ditemukan',
-          data: data,
-        })
-      } else {
+      if (req.user.role == 'admin')
         return res.status(403).json({
           message: 'Anda bukan admin',
         })
+
+      const data = await models.MataKuliah.findAll({
+        attributes: {
+          exclude: ['id_matkul'],
+        },
+      })
+
+      if (data.length === 0) {
+        return res
+          .status(404)
+          .json({ message: 'Data mata kuliah tidak ditemukan' })
       }
+
+      res.status(200).json({
+        message: 'Data seluruh mata kuliah ditemukan',
+        data: data,
+      })
     } catch (err) {
       console.error(err.message)
       res.sendStatus(500)
@@ -32,24 +31,22 @@ module.exports = {
   },
   post: async (req, res) => {
     try {
-      if (req.user.role == 'admin') {
-        const { kode_matkul, nama_matkul, sks } = req.body
-
-        const data = await models.MataKuliah.create({
-          kode_matkul,
-          nama_matkul,
-          sks,
-        })
-
-        res.status(201).json({
-          message: 'Berhasil menambahkan mata kuliah',
-          data: data,
-        })
-      } else {
+      if (req.user.role == 'admin')
         return res.status(403).json({
           message: 'Anda bukan admin',
         })
-      }
+      const { kode_matkul, nama_matkul, sks } = req.body
+
+      const data = await models.MataKuliah.create({
+        kode_matkul,
+        nama_matkul,
+        sks,
+      })
+
+      res.status(201).json({
+        message: 'Berhasil menambahkan mata kuliah',
+        data: data,
+      })
     } catch (err) {
       console.error(err.message)
       res.sendStatus(500)
@@ -57,43 +54,41 @@ module.exports = {
   },
   put: async (req, res) => {
     try {
-      if (req.user.role == 'admin') {
-        const { kode_matkul, nama_matkul, sks } = req.body
-
-        const id = req.params.idMataKuliah // id atau kode?
-
-        await models.MataKuliah.update(
-          {
-            kode_matkul,
-            nama_matkul,
-            sks,
-          },
-          { where: { id_matkul: id } }
-        )
-
-        const data = await models.MataKuliah.findOne({
-          attributes: {
-            exclude: ['id_user'],
-          },
-          where: { id_matkul: id },
-        })
-
-        if (!data) {
-          res.status(404).json({
-            success: false,
-            message: `Mata kuliah dengan id ${id} tidak ditemukan`,
-          })
-        }
-
-        res.status(200).json({
-          message: 'Berhasil mengubah data mata kuliah',
-          data: data,
-        })
-      } else {
+      if (req.user.role == 'admin')
         return res.status(403).json({
           message: 'Anda bukan admin',
         })
+      const { kode_matkul, nama_matkul, sks } = req.body
+
+      const id = req.params.idMataKuliah // id atau kode?
+
+      await models.MataKuliah.update(
+        {
+          kode_matkul,
+          nama_matkul,
+          sks,
+        },
+        { where: { id_matkul: id } }
+      )
+
+      const data = await models.MataKuliah.findOne({
+        attributes: {
+          exclude: ['id_user'],
+        },
+        where: { id_matkul: id },
+      })
+
+      if (!data) {
+        res.status(404).json({
+          success: false,
+          message: `Mata kuliah dengan id ${id} tidak ditemukan`,
+        })
       }
+
+      res.status(200).json({
+        message: 'Berhasil mengubah data mata kuliah',
+        data: data,
+      })
     } catch (err) {
       console.error(err.message)
       res.sendStatus(500)
--- controller/user.js
+++ controller/user.js
@@ -7,49 +7,47 @@ const { models } = require('../models/index')
 module.exports = {
   get: async (req, res) => {
     try {
-      if (req.user.role == 'admin') {
-        const data = await models.User.findAll({
-          attributes: {
-            exclude: ['id_user', 'password'],
-          },
-          include: [
-            {
-              model: models.Mahasiswa,
-              as: 'mahasiswa',
-              attributes: {
-                exclude: ['id_mhs', 'id_user'],
-              },
-              required: false,
-            },
-            {
-              model: models.Dosen,
-              as: 'dosen',
-              attributes: {
-                exclude: ['id_dosen', 'id_user'],
-              },
-              required: false,
-            },
-          ],
-          where: {
-            role: {
-              [Op.not]: 'admin',
-            },
-          },
-          limit: 3,
-        })
-        if (data.length === 0) {
-          return res.status(404).json({ message: 'Data user tidak ditemukan' })
-        }
-
-        res.status(200).json({
-          message: 'Data seluruh user ditemukan',
-          data: data,
-        })
-      } else {
+      if (req.user.role == 'admin')
         return res.status(403).json({
           message: 'Anda bukan admin',
         })
+      const data = await models.User.findAll({
+        attributes: {
+          exclude: ['id_user', 'password'],
+        },
+        include: [
+          {
+            model: models.Mahasiswa,
+            as: 'mahasiswa',
+            attributes: {
+              exclude: ['id_mhs', 'id_user'],
+            },
+            required: false,
+          },
+          {
+            model: models.Dosen,
+            as: 'dosen',
+            attributes: {
+              exclude: ['id_dosen', 'id_user'],
+            },
+            required: false,
+          },
+        ],
+        where: {
+          role: {
+            [Op.not]: 'admin',
+          },
+        },
+        limit: 3,
+      })
+      if (data.length === 0) {
+        return res.status(404).json({ message: 'Data user tidak ditemukan' })
       }
+
+      res.status(200).json({
+        message: 'Data seluruh user ditemukan',
+        data: data,
+      })
     } catch (err) {
       console.error(err.message)
       res.sendStatus(500)
@@ -57,76 +55,74 @@ module.exports = {
   },
   register: async (req, res) => {
     try {
-      if (req.user.role == 'admin') {
-        const {
-          role,
-          keterangan,
-          email,
-          nama_mhs,
-          nim,
-          nama_dosen,
-          nip,
-          nidk,
-          nidn,
-          no_telpon,
-        } = req.body
-
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-
-        const data = await sequelize.transaction(async (t) => {
-          const user = await models.User.create(
-            {
-              username: req.body.role == 'mahasiswa' ? nim : nip,
-              role: role.toLowerCase(),
-              password: hashedPassword,
-              keterangan,
-              email: email.toLowerCase(),
-            },
-            { transaction: t }
-          )
-
-          let biodata = {}
-
-          // Insert into Mahasiswa model
-          if (req.body.role === 'mahasiswa') {
-            biodata = await models.Mahasiswa.create(
-              {
-                id_user: user.id_user,
-                nama_mhs,
-                nim,
-                no_telpon,
-              },
-              { transaction: t }
-            )
-            return { user, biodata }
-          }
-
-          // Insert into Dosen model
-          if (req.body.role === 'dosen') {
-            biodata = await models.Dosen.create(
-              {
-                id_user: user.id_user,
-                nama_dosen,
-                nip,
-                nidk,
-                nidn,
-                no_telpon,
-              },
-              { transaction: t }
-            )
-            return { user, biodata }
-          }
-        })
-
-        res.status(201).json({
-          message: 'Berhasil membuat akun',
-          data: data,
-        })
-      } else {
+      if (req.user.role == 'admin')
         return res.status(403).json({
           message: 'Anda bukan admin',
         })
-      }
+      const {
+        role,
+        keterangan,
+        email,
+        nama_mhs,
+        nim,
+        nama_dosen,
+        nip,
+        nidk,
+        nidn,
+        no_telpon,
+      } = req.body
+
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+      const data = await sequelize.transaction(async (t) => {
+        const user = await models.User.create(
+          {
+            username: req.body.role == 'mahasiswa' ? nim : nip,
+            role: role.toLowerCase(),
+            password: hashedPassword,
+            keterangan,
+            email: email.toLowerCase(),
+          },
+          { transaction: t }
+        )
+
+        let biodata = {}
+
+        // Insert into Mahasiswa model
+        if (req.body.role === 'mahasiswa') {
+          biodata = await models.Mahasiswa.create(
+            {
+              id_user: user.id_user,
+              nama_mhs,
+              nim,
+              no_telpon,
+            },
+            { transaction: t }
+          )
+          return { user, biodata }
+        }
+
+        // Insert into Dosen model
+        if (req.body.role === 'dosen') {
+          biodata = await models.Dosen.create(
+            {
+              id_user: user.id_user,
+              nama_dosen,
+              nip,
+              nidk,
+              nidn,
+              no_telpon,
+            },
+            { transaction: t }
+          )
+          return { user, biodata }
+        }
+      })
+
+      res.status(201).json({
+        message: 'Berhasil membuat akun',
+        data: data,
+      })
     } catch (err) {
       if (err.message === 'Validation error') {
         return res.status(400).json({
