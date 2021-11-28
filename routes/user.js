ea55bd03fd326ea71d6361d77f9c66c55ed6d411
nugraha-abd <nugrahaabdurahman@gmail.com>
2021-11-28 22:05:59 +0700

    Remove checkDuplicate middleware

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const { checkDuplicate } = require('../middleware/checkDuplicate')
-
 const userController = require('../controller/user')
 
 router
   .route('/')
   .get(userController.get)
-  .post(checkDuplicate, userController.register)
+  .post(userController.register)
 
 router
   .route('/:idUser').put(userController.put)
